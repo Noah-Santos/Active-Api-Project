620c86fb5f324e1e92c6ade8823ce93e45d12a68
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2023-10-19 15:59:01 -0700

    First API
    Got the first api to work

---
+++ test.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://store-groceries.p.rapidapi.com/groceries/search/coffee',
+  headers: {
+    'X-RapidAPI-Key': '380492f9b7msh20d59cdef807ed1p1e8de2jsn02e4e09b5a66',
+    'X-RapidAPI-Host': 'store-groceries.p.rapidapi.com'
+  }
+};
+
+let values;
+
+async function getFood(){
+    try {
+        // gets all the data from the api
+        const response = await axios.request(options);
+        values = response.data.map(item=>{
+            let temp = [];
+            // gets the images and assigns them to a temporary variable
+            for(let i = 0; i < item.images.length; i++){
+                temp.push(item.images[i].url);
+            }
+            // returns an array of useful information from the grocery item
+            return [`${item.name}`,`${item.description}`,`${item.manufacturer}`,`${item.price.value}`,temp]
+        });
+        console.log(values);
+        // console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getFood();
