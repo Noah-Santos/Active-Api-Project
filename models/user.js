299e69edebd66706cd497a214f64688c3e3fba11
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2023-10-19 15:20:11 -0700

    Welcome Page UI
    Developed the UI on the welcome page for when the user enters the site and begins viewing.

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const UserSchema  = new mongoose.Schema({
+    first_name :{
+        type : String,
+        required : true
+    } ,
+    last_name:{
+        type : String,
+        required : true
+    } ,
+    email:{
+    type : String,
+    required : true
+    } ,
+    password:{
+        type : String,
+        required : true
+    } ,
+    date :{
+        type: Date,
+        default : Date.now
+    },
+    games :{
+        type : Number,
+        default : 0
+    },
+    wins :{
+        type : Number,
+        default : 0
+    },
+    loses :{
+        type : Number,
+        default : 0
+    },
+    ratio :{
+        type : String,
+        default : '0'
+    }
+},{collection : 'Users'});
+const User= mongoose.model('User',UserSchema);
+
+module.exports = User;
